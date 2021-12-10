c15fb8a677e32fdb45f67c7d1430e15cd7df352c
Roberto <roberto.josephselim@gmail.com>
2021-12-10 15:57:27 +0200

    Editing register system and user login, Added mobileNumber to Students, Using scope identity to relate to IDs

--- user_stories.sql
+++ user_stories.sql
@@ -11,16 +11,18 @@ INSERT INTO PostGradUser
     (email,password)
 VALUES
     (@email, @password)
+declare @id int = SCOPE_IDENTITY();
+
 if @GucianBit =1 
   INSERT INTO GUCianStudent
-    (firstName,lastName,faculty,type,address)
+    (id,firstName,lastName,faculty,type,address)
 VALUES
-    (@first_name, @last_name, @faculty, @GucianBit, @address)
+    (@id,@first_name, @last_name, @faculty, @GucianBit, @address)
   else 
    INSERT INTO NonGUCianStudent
-    (firstName,lastName,faculty,type,address)
+    (id,firstName,lastName,faculty,type,address)
 VALUES
-    (@first_name, @last_name, @faculty, @GucianBit, @address)
+    (@id,@first_name, @last_name, @faculty, @GucianBit, @address)
 
 GO
 CREATE PROC SupervisorRegister
@@ -33,25 +35,53 @@ INSERT INTO PostGradUser
     (email,password)
 VALUES
     (@email, @password)
+declare @id int  = scope_iDentity();
+
 INSERT INTO Supervisor
-    (name,faculty)
+    (id,name,faculty)
 VALUES
-    (@name, @faculty)
+    (@id,@name, @faculty)
 
 
 
--- Leaving Success Bit --
+--We have to ask for it--
 GO
 CREATE PROC userLogin
     @ID int,
-    @paswword varchar(20)
+    @paswword varchar(20),
+    @Success bit OUTPUT
 AS
+if exists(select * from PostGradUser where PostGradUser.id = @id)
+BEGIN
+    set @Success = 1
 
-select *
-from PostGradUser
-where @iD = PostGradUser.id AND @paswword = PostGradUser.password;
+end 
+else 
+BEGIN
+set @Success = 0;
+END
+/*
+we have to test it
+declare @success bit
+exe userlogin 18,'123',@success output
+print @success
+*/
 
--- Leave addMobile Now--
+
+
+GO
+CREATE PROC addMobile
+@ID int,
+@mobile_number varchar(20)
+AS
+IF EXISTS (select * from GUCStudentPhoneNumber where GUCStudentPhoneNumber.GUCianID = @ID )
+BEGIN
+insert into GUCStudentPhoneNumber(GUCianID , phoneNumber) VALUES (@id, @mobile_number);
+END
+ELSE
+BEGIN
+insert into NonGUCianPhoneNumber(NonGUCianID,phoneNumber)VALUES(@id,@mobile_number);
+end;
 
 
 
@@ -59,7 +89,8 @@ GO
 CREATE PROC AdminListUp
 AS
 select *
-from Supervisor;
+from Supervisor
+INNER JOIN PostGradUser ON PostGradUser.id = Supervisor.id;
 
 GO
 CREATE PROC AdminViewSupervisorProfile
@@ -67,6 +98,7 @@ CREATE PROC AdminViewSupervisorProfile
 AS
 select *
 from Supervisor
+INNER JOIN PostGradUser on PostGradUser.id = Supervisor.id
 Where Supervisor.id = @supID;
 
 
@@ -122,174 +154,7 @@ SET Thesis.noExtension = Thesis.noExtension+1
 Where  Thesis.noExtension = @ThesisSerial
 
 
--- 3)h) wait to understand
-
-GO
-CREATE PROC AdminIssueThesisPayment
-    @ThesisSerial INT,
-    @amount DECIMAL,
-    @noOfInstallments INT,
-    @fundPrecentage Decimal
-AS
-Declare @getIdOfThesis SMALLINT
-
-
---3)i) wait to understand
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-GO
-CREATE PROC StudentRegister
-    @first_name varchar(20),
-    @last_name varchar(20),
-    @password varchar(20),
-    @faculty varchar(20),
-    @GucianBit BIT,
-    @email varchar(50),
-    @address varchar(20)
-AS
-INSERT INTO PostGradUser
-    (email,password)
-VALUES
-    (@email, @password)
-if @GucianBit =1 
-  INSERT INTO GUCianStudent
-    (firstName,lastName,faculty,type,address)
-VALUES
-    (@first_name, @last_name, @faculty, @GucianBit, @address)
-  else 
-   INSERT INTO NonGUCianStudent
-    (firstName,lastName,faculty,type,address)
-VALUES
-    (@first_name, @last_name, @faculty, @GucianBit, @address)
-
-GO
-
-CREATE PROC SupervisorRegister
-    @name varchar(20),
-    @password varchar(20),
-    @faculty varchar(20),
-    @email varchar(50)
-AS
-INSERT INTO PostGradUser
-    (email,password)
-values(@email, @password)
-Insert INTO Supervisor
-    (name,faculty)
-values(@name , @faculty)
-
-
-
-
-
-
--- Leaving Success Bit --
-GO
-CREATE PROC userLogin
-    @ID int,
-    @paswword varchar(20)
-AS
-
-select *
-from PostGradUser
-where @iD = PostGradUser.id AND @paswword = PostGradUser.password;
-
--- Leave addMobile Now--
-
-
-
-GO
-CREATE PROC AdminListUp
-AS
-select *
-from Supervisor;
-
-GO
-CREATE PROC AdminViewSupervisorProfile
-    @supID INT
-AS
-select *
-from Supervisor
-Where Supervisor.id = @supID;
-
-
-GO
-CREATE PROC AdminViewAllTheses
-AS
-select *
-from Thesis;
-
-GO
-CREATE PROC AdminViewOnGoingTheses
-    @ThesisCount SMALLINT Output
-AS
-select @ThesisCount = Count(*)
-from Thesis T
-
-
-
-GO
-CREATE PROC AdminViewStudentThesisBySupervisor
-AS
-    Select S1.name, T1.title, GUCianStudent.firstName , GUCianStudent.lastName
-    From GUCianRegisterThesis
-        INNER JOIN Supervisor S1 On S1.id = GUCianRegisterThesis.supervisor_id
-        INNER join Thesis T1 on T1.serialNumber = GUCianRegisterThesis.thesis_id
-        INNER JOIn GUCianStudent ON GUCianStudent.id = GUCianRegisterThesis.GUCianID
-UNION
-    Select S2.name, T2.title, NonGUCianStudent.firstName , NonGUCianStudent.lastName
-    From NonGUCianRegisterThesis
-        INNER JOIN Supervisor S2 On S2.id = NonGUCianRegisterThesis.supervisor_id
-        INNER join Thesis T2 on T2.serialNumber = NonGUCianRegisterThesis.thesis_id
-        INNER JOIn NonGUCianStudent ON NonGUCianStudent.id = NonGUCianRegisterThesis.NonGUCianID
-
-
-
-
-
-GO
-CREATE PROC AdminListNonGucianCourse
-    @courseID Int
-AS
-Select NG.firstName , NG.lastName, C.code, NonGUCianTakeCourse.grade
-FRom NonGUCianTakeCourse
-    INNER JOIN NonGUCianStudent NG ON NG.id = NonGUCianTakeCourse.NonGUCianID
-    INNER JOIN Course C ON C.id = NonGUCianTakeCourse.course_id
-
-GO
-CREATE PROC AdminUpdateExtension
-    @ThesisSerial INT
-AS
-UPDATE Thesis 
-SET Thesis.noExtension = Thesis.noExtension+1
-Where  Thesis.noExtension = @ThesisSerial
-
-
--- 3)h) wait to understand
+-- 3)h) hassan did it
 
 GO
 CREATE PROC AdminIssueThesisPayment
@@ -309,11 +174,11 @@ CREATE PROC AdminViewStudentProfile
 AS
 If EXISTS (Select * From GUCianStudent where GUCianStudent.id = @sid)
 begin
-select * from GUCianStudent;
+select * from GUCianStudent INNER JOIN PostGradUser ON PostGradUser.id = GUCianStudent.id;
 end
 else 
 begin
-select * from NonGUCianStudent;
+select * from NonGUCianStudent INNER JOIN PostGradUser ON PostGradUser.id = NonGUCianStudent.id;
 end
 
 
