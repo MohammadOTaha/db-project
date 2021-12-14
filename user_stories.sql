29e71891efd5f2ef0e59f7ff9f8020b9e8414605
MohammadOTaha <mohammadomartaha17@gmail.com>
2021-12-14 17:36:13 +0200

    add: section (4) user stories

--- user_stories.sql
+++ user_stories.sql
@@ -1,4 +1,3 @@
-
 GO
 CREATE PROC StudentRegister
     @first_name varchar(20),
@@ -333,47 +332,222 @@ UNION
         WHEre ExaminerEvaluateDefense.[date] = @defenseDate
 
 
---4)e)--
-go
-create proc AddDefenseNonGucian
-    @ThesisSerialNo int ,
-    @DefenseDate Datetime ,
-    @DefenseLocation varchar(15)
-as
-IF @ThesisSerialNo is null or @DefenseDate is null or @DefenseLocation is null
+------------------- (4) Supservisor's Features -------------------
+
+-- 4.a: Evaluate a student’s progress report, and give evaluation value 0 to 3
+GO
+CREATE PROC EvaluateProgressReport
+    @supervisorID INT,
+    @thesisSerialNo INT,
+    @progressReportNo INT,
+    @evaluation INT
+AS
+IF EXISTS (
+    SELECT * FROM GUCianProgressReport PR
+    WHERE 
+        PR.thesisSerialNumber = @thesisSerialNo AND 
+        PR.progressReportNumber = @progressReportNo AND
+        PR.supervisor_id = @supervisorID
+)
 BEGIN
-    print 'hello'
+    UPDATE GUCianProgressReport
+    SET evaluation = @evaluation
+    WHERE 
+        GUCianProgressReport.thesisSerialNumber = @thesisSerialNo AND 
+        GUCianProgressReport.progressReportNumber = @progressReportNo AND
+        GUCianProgressReport.supervisor_id = @supervisorID
 END
-else 
+ELSE
 BEGIN
-    declare @id int
-    select @id = NonGUCianRegisterThesis.NonGUCianID
-    from NonGUCianRegisterThesis
-    where NonGUCianRegisterThesis.thesisSerialNumber = @ThesisSerialNo
-    if @id IN (                 select id
-        from NonGUCianStudent
-    EXCEPT
-        select s.id
-        from NonGUCianStudent S, Course C , NonGUCianTakeCourse X
-        where S.id = X.NonGUCianID and C.id = X.NonGUCianID and X.grade <= 50)
-        insert into Defense
-        (thesisSerialNumber, date, location)
-    values
-        (@ThesisSerialNo, @DefenseDate, @DefenseLocation)
+    UPDATE NonGUCianProgressReport
+    SET evaluation = @evaluation
+    WHERE 
+        NonGUCianProgressReport.thesisSerialNumber = @thesisSerialNo AND 
+        NonGUCianProgressReport.progressReportNumber = @progressReportNo AND
+        NonGUCianProgressReport.supervisor_id = @supervisorID
 END
 
-/*
-drop proc AddDefenseNonGucian
-exec AddDefenseNonGucian 1, '2022-05-07', 'KOKO';
-select *
-from Defense;
-select * from NonGUCianStudent
-Select * from NonGUCianTakeCourse
--- Testing some stuff --
-*/
+-- 4.b: View all my students’s names and years spent in the thesis.
+GO
+CREATE PROC ViewSupStudentsYears
+    @supervisorID INT
+AS
+BEGIN
+    SELECT GUCianStudent.firstName, GUCianStudent.lastName, T1.years
+    FROM GUCianRegisterThesis GUCianThesis
+    INNER JOIN GUCianStudent GUCianStudent ON GUCianStudent.id = GUCianThesis.GUCianID
+    INNER JOIN Thesis T1 ON T1.serialNumber = GUCianThesis.thesisSerialNumber
+    WHERE GUCianThesis.supervisor_id = @supervisorID
+    UNION
+    SELECT NonGUCianStudent.firstName, NonGUCianStudent.lastName, T2.years
+    FROM NonGUCianRegisterThesis NonGUCianThesis
+    INNER JOIN NonGUCianStudent NonGUCianStudent ON NonGUCianStudent.id = NonGUCianThesis.NonGUCianID
+    INNER JOIN Thesis T2 ON T2.serialNumber = NonGUCianThesis.thesisSerialNumber
+    WHERE NonGUCianThesis.supervisor_id = @supervisorID
+END
 
+-- 4.c: View my profile and update my personal information.
+GO
+CREATE PROC SupViewProfile
+    @supervisorID INT
+AS
+BEGIN
+    SELECT * FROM Supervisor
+    WHERE Supervisor.id = @supervisorID
+END
 
+GO
+CREATE PROC UpdateSupProfile
+    @supervisorID INT,
+    @name VARCHAR(20),
+    @faculty VARCHAR(20)
+AS
+BEGIN
+    UPDATE Supervisor
+    SET name = @name, faculty = @faculty
+    WHERE Supervisor.id = @supervisorID
+END
 
+-- 4.d: View all publications of a student.
+GO
+CREATE PROC ViewAStudentPublications
+    @studentID INT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT * FROM GUCianStudent
+        WHERE GUCianStudent.id = @studentID
+    )
+    BEGIN
+        SELECT P.title
+        FROM GUCianStudent GUCianStudent
+        INNER JOIN GUCianRegisterThesis GUCianRegisterThesis ON GUCianRegisterThesis.GUCianID = GUCianStudent.id
+        INNER JOIN Thesis_Publication T_P ON T_P.thesisSerialNumber = GUCianRegisterThesis.thesisSerialNumber
+        INNER JOIN Publication P ON P.id = T_P.publication_id
+        WHERE GUCianStudent.id = @studentID
+    END
+    ELSE
+    BEGIN
+        SELECT P.title
+        FROM NonGUCianStudent NonGUCianStudent
+        INNER JOIN NonGUCianRegisterThesis NonGUCianRegisterThesis ON NonGUCianRegisterThesis.NonGUCianID = NonGUCianStudent.id
+        INNER JOIN Thesis_Publication T_P ON T_P.thesisSerialNumber = NonGUCianRegisterThesis.thesisSerialNumber
+        INNER JOIN Publication P ON P.id = T_P.publication_id
+        WHERE NonGUCianStudent.id = @studentID
+    END
+END
+
+-- 4.e: Add defense for a thesis, for nonGucian students all courses’ grades should be greater than 50 percent.
+-- TODO: not sure about not having grade
+GO
+CREATE PROC AddDefenseGucian
+    @thesisSerialNo INT,
+    @defenseDate DATETIME,
+    @defenseLocation VARCHAR(15) -- TODO: Shall we change the length to 15 in table_creations too? 
+AS
+BEGIN
+    INSERT INTO Defense (thesisSerialNumber, date, location)
+    VALUES (@thesisSerialNo, @defenseDate, @defenseLocation)
+
+    UPDATE Thesis
+    SET defenseDate = @defenseDate
+    WHERE Thesis.serialNumber = @thesisSerialNo
+END
+
+GO
+CREATE PROC AddDefenseNonGucian
+    @thesisSerialNo INT,
+    @defenseDate DATETIME,
+    @defenseLocation VARCHAR(15) -- TODO: Shall we change the length to 15 in table_creations too?
+AS
+BEGIN
+    -- get NonGUCianID
+    DECLARE @NonGUCianID INT
+    SELECT @NonGUCianID = NonGUCianID
+    FROM NonGUCianRegisterThesis T
+    WHERE T.thesisSerialNumber = @thesisSerialNo
+
+    -- check if all student's grades are greater than 50 percent
+    IF NOT EXISTS (
+        SELECT * FROM NonGUCianTakeCourse C
+        WHERE C.NonGUCianID = @NonGUCianID AND C.grade <= 50
+    )
+    BEGIN
+        INSERT INTO Defense (thesisSerialNumber, date, location)
+        VALUES (@thesisSerialNo, @defenseDate, @defenseLocation)
+
+        UPDATE Thesis
+        SET defenseDate = @defenseDate
+        WHERE Thesis.serialNumber = @thesisSerialNo
+    END
+END
+
+-- 4.f: Add examiner(s) for a defense.
+GO
+CREATE PROC AddExaminer
+    @thesisSerialNo INT,
+    @DefenseDate DATETIME,
+    @ExaminerName VARCHAR(20),
+    @National BIT,
+    @fieldOfWork VARCHAR(20)
+AS
+BEGIN
+    INSERT INTO Examiner(name, isNational, fieldOfWork)
+    VALUES (@ExaminerName, @National, @fieldOfWork)
+    
+    DECLARE @ExaminerID INT = SCOPE_IDENTITY()
+    INSERT INTO ExaminerEvaluateDefense(thesisSerialNumber, examiner_id, date)
+    VALUES (@thesisSerialNo, @ExaminerID, @DefenseDate)
+END
+
+-- 4.g: Cancel a Thesis if the evaluation of the last progress report is zero.
+GO
+CREATE PROC CancelThesis
+    @thesisSerialNo INT
+AS
+BEGIN
+    IF ( 
+        EXISTS (
+            SELECT * FROM GUCianProgressReport PR
+            WHERE PR.thesisSerialNumber = @thesisSerialNo
+        ) AND (
+            SELECT TOP 1 evaluation FROM GUCianProgressReport PR
+            WHERE PR.thesisSerialNumber = @thesisSerialNo
+            ORDER BY PR.date DESC
+        ) = 0
+    )
+    BEGIN
+        DELETE FROM Thesis
+        WHERE Thesis.serialNumber = @thesisSerialNo
+    END
+    ELSE IF (
+        EXISTS (
+            SELECT * FROM NonGUCianProgressReport PR
+            WHERE PR.thesisSerialNumber = @thesisSerialNo
+        ) AND (
+            SELECT TOP 1 evaluation FROM NonGUCianProgressReport PR
+            WHERE PR.thesisSerialNumber = @thesisSerialNo
+            ORDER BY PR.date DESC
+        ) = 0
+    )
+    BEGIN
+        DELETE FROM Thesis
+        WHERE Thesis.serialNumber = @thesisSerialNo
+    END
+END
+
+-- 4.h: Add a grade for a thesis.
+-- TODO: grade missing in question statement!
+GO
+CREATE PROC AddGrade
+    @thesisSerialNo INT,
+    @grade DECIMAL
+AS
+BEGIN
+    UPDATE Thesis
+    SET grade = @grade
+    WHERE Thesis.serialNumber = @thesisSerialNo
+END
 
 --Hassan Solutions
 GO
