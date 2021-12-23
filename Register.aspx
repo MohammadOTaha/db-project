bfd1a273d58091086f29cdf99c357bbec0d16126
MohammadOTaha <mohammadomartaha17@gmail.com>
2021-12-23 22:03:58 +0200

    add: Register web form w/ registering logic

---
+++ Register.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="PostGradSystem.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label_fn" runat="server" Text="First Name: "></asp:Label>
+            <asp:TextBox ID="firstname" runat="server" Text=""></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_ln" runat="server" Text="Last Name: "></asp:Label>
+            <asp:TextBox ID="lastname" runat="server" Text=""></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_address" runat="server" Text="Address: "></asp:Label>
+            <asp:TextBox ID="address" runat="server" Text=""></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_email" runat="server" Text="Email: "></asp:Label>
+            <asp:TextBox ID="email" runat="server" Text="" name="em"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_pass" runat="server" Text="Password: "></asp:Label>
+            <asp:TextBox ID="password" runat="server" Text=""></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_fac" runat="server" Text="Faculty: "></asp:Label>
+            <asp:TextBox ID="faculty" runat="server" Text=""></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_utype" runat="server" Text="User Type: "></asp:Label>
+            <asp:DropDownList ID="usertypedroplist" runat="server" AutoPostBack="False">
+                <asp:ListItem>GUCian Student</asp:ListItem>
+                <asp:ListItem>Non-GUCian Student</asp:ListItem>
+                <asp:ListItem>Admin</asp:ListItem>
+                <asp:ListItem>Examiner</asp:ListItem>
+                <asp:ListItem>Supervisor</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+
+
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Register" OnClick="register"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Collections.Generic;
+
+namespace PostGradSystem
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static DBConnection db_connection = new DBConnection();
+        private class DBConnection
+        {
+            private static SqlConnection conn;
+
+            public DBConnection() 
+            {
+                conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            }
+
+            public SqlConnection getConnection()
+            {
+                return conn;
+            }
+        }
+
+        private static void registerStudent(bool is_gucian, Dictionary<String, String> user_info)
+        {
+            SqlCommand register;
+            if(is_gucian) {
+                register = new SqlCommand(
+                    "INSERT INTO GUCianStudent (id, first_name, last_name, type, faculty, address, underGradID) VALUES (@id, @first_name, @last_name, @type, @faculty, @address, @underGradID)", 
+                    db_connection.getConnection()
+                );
+            }
+            else {
+                register = new SqlCommand(
+                    "INSERT INTO NonGUCianStudent (id, first_name, last_name, type, faculty, address) VALUES (@id, @first_name, @last_name, @type, @faculty, @address)", 
+                    db_connection.getConnection()
+                );
+            }
+
+            register.Parameters.AddWithValue("@id", user_info["user_id"]);
+            register.Parameters.AddWithValue("@first_name", user_info["first_name"]);
+            register.Parameters.AddWithValue("@last_name", user_info["last_name"]);
+            register.Parameters.AddWithValue("@type", user_info["type"]);
+            register.Parameters.AddWithValue("@faculty", user_info["faculty"]);
+            register.Parameters.AddWithValue("@address", user_info["address"]);
+            if(is_gucian) register.Parameters.AddWithValue("@underGradID", user_info["underGradID"]);
+
+            register.ExecuteNonQuery();
+        }
+
+        private static void registerSupervisor(Dictionary<String, String> user_info) 
+        {
+            SqlCommand register = new SqlCommand(
+                "INSERT INTO Supervisor (id, name, faculty) VALUES (@id, @name, @faculty)", 
+                db_connection.getConnection()
+            );
+
+            String name = user_info["first_name"] + " " + user_info["last_name"];
+            register.Parameters.AddWithValue("@id", user_info["user_id"]);
+            register.Parameters.AddWithValue("@name", name);
+            register.Parameters.AddWithValue("@faculty", user_info["faculty"]);
+
+            register.ExecuteNonQuery();
+        }
+
+        private static void registerExaminer(Dictionary<String, String> user_info)
+        {
+            SqlCommand register = new SqlCommand(
+                "INSERT INTO Examiner (id, name, fieldOfWork, isNational) VALUES (@id, @name, @fieldOfWork, @isNational)", 
+                db_connection.getConnection()
+            );
+
+            String name = user_info["first_name"] + " " + user_info["last_name"];
+            register.Parameters.AddWithValue("@id", user_info["user_id"]);
+            register.Parameters.AddWithValue("@name", name);
+            register.Parameters.AddWithValue("@fieldOfWork", user_info["fieldOfWork"]);
+            register.Parameters.AddWithValue("@isNational", user_info["isNational"]);
+
+            register.ExecuteNonQuery();
+        }
+
+        private static void registerAdmin(Dictionary<String, String> user_info)
+        {
+            SqlCommand register = new SqlCommand(
+                "INSERT INTO Admin (id) VALUES (@id)", 
+                db_connection.getConnection()
+            );
+
+            register.Parameters.AddWithValue("@id", user_info["user_id"]);
+
+            register.ExecuteNonQuery();
+        }
+
+        protected void register(object sender, EventArgs e)
+        {
+            Dictionary<String, String> user_info = new Dictionary<String, String>();
+            user_info.Add("first_name", firstname.Text);
+            user_info.Add("last_name", lastname.Text);
+            user_info.Add("address", address.Text);
+            user_info.Add("email", email.Text);
+            user_info.Add("password", password.Text);
+            user_info.Add("faculty", faculty.Text);
+            user_info.Add("type", usertypedroplist.SelectedValue);
+
+            SqlCommand insertUser = new SqlCommand("INSERT INTO PostGradUser (email, password) VALUES (@email, @password) SELECT scope_identity()", db_connection.getConnection());
+            insertUser.Parameters.AddWithValue("@email", user_info["email"]);
+            insertUser.Parameters.AddWithValue("@password", user_info["password"]);
+
+            db_connection.getConnection().Open();
+            
+            int user_id = Convert.ToInt32(insertUser.ExecuteScalar());
+            user_info.Add("user_id", user_id.ToString());
+
+            switch(user_info["type"]) {
+                case "GUCian Student":
+                    registerStudent(true, user_info);
+                    break;
+                case "Non-GUCian Student":
+                    registerStudent(false, user_info);
+                    break;
+                case "Supervisor":
+                    registerSupervisor(user_info);
+                    break;
+                case "Examiner":
+                    registerExaminer(user_info);
+                    break;
+                case "Admin":
+                    registerAdmin(user_info);
+                    break;
+            }
+            
+            db_connection.getConnection().Close();
+        }
+    }
+}
