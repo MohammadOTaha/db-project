65609a843a9ca5d83863ac9d07c67562a6c362ac
MohammadOTaha <mohammadomartaha17@gmail.com>
2021-12-23 22:08:07 +0200

    add: Publications web form w/ adding & linking logic

---
+++ AddPublication.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddPublication.aspx.cs" Inherits="PostGradSystem.AddPublication" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl_pubTitle" runat="server" Text="Publication Title: "></asp:Label>
+            <asp:TextBox ID="pub_title" runat="server" Text=""></asp:TextBox>
+        </div>
+
+        <div>
+            <asp:Label ID="lbl_pubDate" runat="server" Text="Publication Date: "></asp:Label>
+            <!-- date input -->
+            <asp:TextBox ID="pub_date" runat="server" textmode="Date"></asp:TextBox>
+        </div>
+
+        <div>
+            <asp:Label ID="lbl_pubHost" runat="server" Text="Publication Host: "></asp:Label>
+            <asp:TextBox ID="pub_host" runat="server" Text=""></asp:TextBox>
+        </div>
+
+        <div>
+            <asp:Label ID="lbl_pubPlace" runat="server" Text="Publication Place: "></asp:Label>
+            <asp:TextBox ID="pub_place" runat="server" Text=""></asp:TextBox>
+        </div>
+
+        <div>  
+            <asp:Label ID="lbl_pubType" runat="server" Text="Publication Accepted: "></asp:Label>
+            <asp:RadioButton ID="rdoYes" runat="server" Text="Yes" GroupName="rdo_accepted" />  
+            <asp:RadioButton ID="rdoNo" runat="server" Text="No" GroupName="rdo_accepted" />  
+        </div> 
+        
+        <asp:panel id="linkPubPanel" runat="server" visible="false">
+            <asp:Label ID="lbl_thesis" runat="server" Text="Thesis: "></asp:Label>
+            <asp:DropDownList ID="thesis_dropList" runat="server" AutoPostBack="False">
+            </asp:DropDownList>
+        </asp:panel>
+        
+        <asp:Button ID="btn_showLinkPub" runat="server" Text="Link Publication to Thesis" OnClick="showLink"></asp:Button>
+        <br/>
+        <br/>
+        <asp:Button ID="btn_addPub" runat="server" Text="Add Publication" OnClick="addPublication"/>
+
+            
+    </form>
+</body>
+</html>
---
+++ AddPublication.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace PostGradSystem
+{
+    public partial class AddPublication : System.Web.UI.Page
+    {
+        static DBConnection db_connection = new DBConnection();
+        private class DBConnection
+        {
+            private static SqlConnection conn;
+
+            public DBConnection() 
+            {
+                conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            }
+
+            public SqlConnection getConnection()
+            {
+                return conn;
+            }
+        }
+        static Dictionary<String, String> thesis_info;
+        static Boolean linkPubToThesis = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static Boolean is_gucian(String user_id)
+        {
+            SqlCommand cmd = new SqlCommand(
+                (
+                    @"
+                    IF EXISTS (SELECT * FROM GUCianStudent WHERE id = @user_id)
+                        BEGIN
+                            SET @is_gucian = 1;
+                        END
+                    ELSE 
+                        BEGIN
+                            SET @is_gucian = 0;
+                        END
+                    "
+                ), 
+                db_connection.getConnection());
+
+            cmd.Parameters.AddWithValue("@user_id", user_id);
+            cmd.Parameters.Add("@is_gucian", System.Data.SqlDbType.Bit);
+            cmd.Parameters["@is_gucian"].Direction = System.Data.ParameterDirection.Output;
+
+            cmd.ExecuteNonQuery();
+
+            return Convert.ToBoolean(cmd.Parameters["@is_gucian"].Value);
+        }
+
+        private static SqlDataReader getStudentTheses(String user_id) 
+        {
+            if(is_gucian(user_id)) {
+                SqlCommand cmd = new SqlCommand(
+                    (
+                        @"
+                        SELECT T.title, T.serialNumber
+                        FROM Thesis T
+                        INNER JOIN GUCianRegisterThesis ST ON T.serialNumber = ST.thesisSerialNumber
+                        WHERE ST.GUCianID = @user_id
+                        "
+                    ), 
+                    db_connection.getConnection()
+                );
+
+                cmd.Parameters.AddWithValue("@user_id", user_id);
+
+                return cmd.ExecuteReader();
+            }
+            else {
+                SqlCommand cmd = new SqlCommand(
+                    (
+                        @"
+                        SELECT T.title, T.serialNumber 
+                        FROM Thesis T
+                        INNER JOIN NonGUCianRegisterThesis ST ON T.serialNumber = ST.thesisSerialNumber
+                        WHERE ST.NonGUCianID = @user_id
+                        "
+                    ), 
+                    db_connection.getConnection()
+                );
+
+                cmd.Parameters.AddWithValue("@user_id", user_id);
+
+                return cmd.ExecuteReader();
+            }
+        }
+        protected void showLink(object sender, EventArgs e)
+        {
+            btn_showLinkPub.Visible = false;
+
+            linkPubToThesis = true;
+            thesis_info = new Dictionary<String, String>();
+
+            int user_id = 6;
+
+            if(user_id == 0) {
+                Response.Redirect("~/Login.aspx");
+            }
+            else {
+                db_connection.getConnection().Open();
+                
+                SqlDataReader reader = getStudentTheses(user_id.ToString());
+
+                while(reader.Read()) {
+                    String thesis_title = reader.GetString(0);
+                    String thesis_serial_number = reader.GetInt32(1).ToString();
+
+                    thesis_info.Add(thesis_title, thesis_serial_number);
+                    thesis_dropList.Items.Add(thesis_title);
+                }
+                
+                linkPubPanel.Visible = true;
+
+                reader.Close();
+                db_connection.getConnection().Close();
+            }
+
+        }
+
+        protected void addPublication(object sender, EventArgs e)
+        {
+            // execute stored procedure
+            SqlCommand add_pub_sp = new SqlCommand("addPublication", db_connection.getConnection());
+            add_pub_sp.CommandType = System.Data.CommandType.StoredProcedure;
+
+            // add parameters
+            add_pub_sp.Parameters.AddWithValue("@title", pub_title.Text);
+            add_pub_sp.Parameters.AddWithValue("@pubDate", pub_date.Text);
+            add_pub_sp.Parameters.AddWithValue("@host", pub_host.Text);
+            add_pub_sp.Parameters.AddWithValue("@place", pub_place.Text);
+            add_pub_sp.Parameters.AddWithValue("@accepted", rdoYes.Checked ? 1 : 0);
+
+            // execute stored procedure
+            db_connection.getConnection().Open();
+            add_pub_sp.ExecuteNonQuery();
+
+            // get the last inserted publication id
+            SqlCommand cmd = new SqlCommand(
+                (
+                    @"
+                    SELECT TOP 1 id
+                    FROM Publication
+                    ORDER BY id DESC
+                    "
+                ), 
+                db_connection.getConnection()
+            );
+
+            int pub_id = Convert.ToInt32(cmd.ExecuteScalar());
+
+            if(linkPubToThesis) {
+                SqlCommand link_pub_thesis_sp = new SqlCommand("linkPubThesis", db_connection.getConnection());
+                link_pub_thesis_sp.CommandType = System.Data.CommandType.StoredProcedure;
+
+                link_pub_thesis_sp.Parameters.AddWithValue("@PubID", pub_id);
+                link_pub_thesis_sp.Parameters.AddWithValue("@thesisSerialNo", thesis_info[thesis_dropList.SelectedItem.Text]);
+
+                link_pub_thesis_sp.ExecuteNonQuery();
+            }
+
+            db_connection.getConnection().Close();
+        }
+    }
+}
