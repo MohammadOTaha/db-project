49fae4efeed6d9a38fa507749c1d8b50057b9356
MohammadOTaha <mohammadomartaha17@gmail.com>
2021-12-23 22:04:36 +0200

    add: Login web form w/ logging in logic

---
+++ Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="PostGradSystem.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label_in_email" runat="server" Text="Email: "></asp:Label>
+            <asp:TextBox ID="in_email" runat="server" Width="200"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="label_in_pass" runat="server" Text="Password: "></asp:Label>
+            <asp:TextBox ID="in_pass" runat="server" Width="200"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="login"></asp:Button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostGradSystem
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login(object sender, EventArgs e)
+        {
+            String input_mail = in_email.Text;
+            String input_pass = in_pass.Text;
+
+            String connString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+            SqlConnection conn = new SqlConnection(connString);
+
+            // get the id of the user with the given email using select statement
+            SqlCommand getId = new SqlCommand("SELECT id FROM PostGradUser WHERE email = @input_mail", conn);
+            getId.Parameters.AddWithValue("@input_mail", input_mail);
+
+            // use the stored procedure to login the user
+            SqlCommand loginProc = new SqlCommand("userLogin", conn);
+            SqlParameter out_bit = new SqlParameter("@success", System.Data.SqlDbType.Bit);
+
+            conn.Open();
+            int id = Convert.ToInt32(getId.ExecuteScalar());
+
+            loginProc.Parameters.AddWithValue("@id", id);
+            loginProc.Parameters.AddWithValue("@password", input_pass);
+            loginProc.Parameters.Add(out_bit);
+            out_bit.Direction = System.Data.ParameterDirection.Output;
+
+            loginProc.CommandType = System.Data.CommandType.StoredProcedure;
+            loginProc.ExecuteNonQuery();
+            bool success = (bool)out_bit.Value;
+            conn.Close();
+
+            Response.Write(success);
+        }
+    }
+}
