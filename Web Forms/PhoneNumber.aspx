9ebf4b983fc3c45ae7e53480b27763ce9d9bfcb9
MohammadOTaha <mohammadomartaha17@gmail.com>
2021-12-24 08:49:55 +0200

    add: PhoneNumber web form w/ adding logic

---
+++ Web Forms/PhoneNumber.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhoneNumber.aspx.cs" Inherits="PostGradSystem.PhoneNumber" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl_phone" runat="server" Text="Phone Numbers:"></asp:Label>
+            <asp:BulletedList id="ul" runat="server"/>
+        </div>
+
+        <div>
+            <asp:Label ID="lbl_addPhone" runat="server" Text="Add Phone Number: "></asp:Label>
+            <asp:TextBox ID="tb_add_phone" runat="server" Text=""></asp:TextBox>
+            <asp:Button ID="btn_addPhone" runat="server" Text="Add" OnClick="AddPhoneNumber"></asp:Button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web Forms/PhoneNumber.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+
+namespace PostGradSystem
+{
+    public partial class PhoneNumber : System.Web.UI.Page
+    {
+
+        static DBConnection db_connection = new DBConnection();
+        private class DBConnection
+        {
+            private static SqlConnection conn;
+
+            public DBConnection() 
+            {
+                conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            }
+
+            public SqlConnection getConnection()
+            {
+                return conn;
+            }
+        }
+        
+        static Boolean isGUCian;
+        private static Boolean is_gucian(String user_id)
+        {
+            SqlCommand cmd = new SqlCommand(
+                (
+                    @"
+                    IF EXISTS (SELECT * FROM GUCianStudent WHERE id = @user_id)
+                        BEGIN
+                            SET @is_gucian = 1;
+                        END
+                    ELSE 
+                        BEGIN
+                            SET @is_gucian = 0;
+                        END
+                    "
+                ), 
+                db_connection.getConnection());
+
+            cmd.Parameters.AddWithValue("@user_id", user_id);
+            cmd.Parameters.Add("@is_gucian", System.Data.SqlDbType.Bit);
+            cmd.Parameters["@is_gucian"].Direction = System.Data.ParameterDirection.Output;
+
+            cmd.ExecuteNonQuery();
+
+            return Convert.ToBoolean(cmd.Parameters["@is_gucian"].Value);
+        }
+
+        private static List<String> getStudentsPhoneNumbers(String user_id) {
+            SqlCommand cmd;
+            if(isGUCian) {
+                cmd = new SqlCommand(
+                    (
+                        @"
+                        SELECT phoneNumber FROM GUCStudentPhoneNumber WHERE GUCianID = @user_id
+                        "
+                    ), 
+                    db_connection.getConnection()
+                );
+            }
+            else {
+                cmd = new SqlCommand(
+                    (
+                        @"
+                        SELECT phoneNumber FROM NonGUCianPhoneNumber WHERE NonGUCianID = @user_id
+                        "
+                    ), 
+                    db_connection.getConnection()
+                );
+            }
+            cmd.Parameters.AddWithValue("@user_id", user_id);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<String> phoneNumbers = new List<String>();
+            while(reader.Read()) {
+                phoneNumbers.Add(reader.GetString(0));
+            }
+            
+            reader.Close();
+
+            return phoneNumbers;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int user_id = Convert.ToInt32(Session["user_id"]);
+
+            if(user_id == 0) {
+                Response.Redirect("~/Login.aspx");
+            }
+            else {
+                db_connection.getConnection().Open();
+
+                isGUCian = is_gucian(user_id.ToString());
+
+                List<String> phoneNumbers = getStudentsPhoneNumbers(user_id.ToString());
+
+                foreach(String phoneNumber in phoneNumbers) {
+                    ListItem item = new ListItem(phoneNumber, phoneNumber);
+                    ul.Items.Add(item);
+                }
+
+                db_connection.getConnection().Close();
+            }
+        }
+
+        protected void AddPhoneNumber(object sender, EventArgs e)
+        {
+            int user_id = Convert.ToInt32(Session["user_id"]);
+            
+            if(!String.IsNullOrEmpty(tb_add_phone.Text)) {
+                db_connection.getConnection().Open();
+
+                SqlCommand addPhone_sp = new SqlCommand("addMobile", db_connection.getConnection());
+                addPhone_sp.CommandType = System.Data.CommandType.StoredProcedure;
+
+                addPhone_sp.Parameters.AddWithValue("@ID", user_id);
+                addPhone_sp.Parameters.AddWithValue("@mobile_number", tb_add_phone.Text);
+
+                addPhone_sp.ExecuteNonQuery();
+
+                db_connection.getConnection().Close();
+
+                Response.Redirect("~/PhoneNumber.aspx");
+            }
+        }
+
+    }
+}
